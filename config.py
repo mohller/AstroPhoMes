98be4d37020047bb2655cee08ee6cbd499754ea9
mohller <leonel.rmh@gmail.com>
2019-03-18 16:31:30 +0100

    Adding the messaging function for debugging and related config level

--- config.py
+++ config.py
@@ -1,9 +1,12 @@
 """ Configuration settings
 """
 import pickle
-
 import os.path as path
+
+
 global_path = path.dirname(path.abspath(__file__))
 
 spec_data = pickle.load(
     open(path.join(global_path, "data/particle_data.ppo"), "rb"))
+
+debug_level = 3 # defines the information output of the code
--- utils/utils.py
+++ utils/utils.py
@@ -2,6 +2,24 @@
 """
 
 import numpy as np
+sys.path.append('../')
+from config import *
+
+def info(min_dbg_level, *message):
+    """Print to console if `min_debug_level <= config["debug_level"]`
+
+    The fuction determines automatically the name of caller and appends
+    the message to it. Message can be a tuple of strings or objects
+    which can be converted to string using `str()`.
+
+    Args:
+        min_dbg_level (int): Minimum debug level in config for printing
+        message (tuple): Any argument or list of arguments that casts to str
+    """
+
+    if min_dbg_level <= debug_level:
+        message = [str(m) for m in message]
+        print caller_name() + " ".join(message)
 
 def get_AZN(nco_id):
     """Returns mass number :math:`A`, charge :math:`Z` and neutron
@@ -21,7 +39,6 @@ def get_AZN(nco_id):
         Z, A = 0, 0
 
     return A, Z, A - Z
-
 
 def bin_widths(bin_edges):
     """Computes and returns bin widths from given edges."""
