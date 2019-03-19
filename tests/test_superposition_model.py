4c97a53ad6e7e1b48715c2a3d802f6b7adfb3fb8
mohller <leonel.rmh@gmail.com>
2019-03-19 08:51:34 +0100

    Improved tests on incl and incl_diff

--- tests/test_superposition_model.py
+++ tests/test_superposition_model.py
@@ -42,20 +42,25 @@ class Test_SuperpositionModel(unittest.TestCase):
         """
             Test that nonel works
         """
+        from scipy.integrate import trapz
+        
         cs_proton = self.pm.cs_proton_grid
         cs_neutron = self.pm.cs_neutron_grid
 
-        e, cs = self.pm.cs_incl(502, 402)
-        cs_val = 3 * cs_neutron
-        self.assertTrue(np.all(cs == cs_val))        
-                
-        e, cs = self.pm.cs_incl(704, 603)
-        cs_val = 4 * cs_proton
-        self.assertTrue(np.all(cs == cs_val))
-        
-        e, cs = self.pm.cs_incl(1407, 402)
-        cs_val = np.zeros_like(cs)
-        self.assertTrue(np.all(cs == cs_val))        
+        redist_p = self.pm.redist_proton
+        redist_n = self.pm.redist_neutron
+
+        for spec, cs_tot, redist in zip([100, 101], 
+            [cs_neutron, cs_proton],
+            [redist_n, redist_p]):
+            for prod in [2, 3, 4, 100, 101]:
+                e, cs = self.pm.cs_incl(spec, prod)
+                cs_val_diff = cs_tot * redist[prod].T
+
+                cs_val = trapz(cs_val_diff, x=self.pm.xcenters,
+                              dx=bin_widths(self.pm.xbins), axis=0)
+
+                self.assertTrue(np.all(cs == cs_val))        
 
     def test_incl(self):
         """
@@ -76,7 +81,25 @@ class Test_SuperpositionModel(unittest.TestCase):
         cs_val = np.zeros_like(cs)
         self.assertTrue(np.all(cs == cs_val))        
 
-    def test_incl_diff(self):
+    def test_incl_diff_nucleons(self):
+        """
+            Test that nonel works
+        """
+        cs_proton = self.pm.cs_proton_grid
+        cs_neutron = self.pm.cs_neutron_grid
+
+        redist_p = self.pm.redist_proton
+        redist_n = self.pm.redist_neutron
+
+        for spec, cs_tot, redist in zip([100, 101], 
+            [cs_neutron, cs_proton],
+            [redist_n, redist_p]):
+            for prod in [2, 3, 4, 100, 101]:
+                e, cs = self.pm.cs_incl_diff(spec, prod)
+                cs_val = cs_tot * redist[prod].T
+                self.assertTrue(np.all(cs == cs_val))        
+
+    def test_incl_diff_nuclei(self):
         """
             Test that nonel works
         """
@@ -98,23 +121,23 @@ class Test_SuperpositionModel(unittest.TestCase):
         self.assertTrue(np.all(cs == cs_val))
 
 
-class Test_EmpiricalModel(Test_SuperpositionModel):
-    def __init__(self, *args, **kwargs):
-        super(Test_EmpiricalModel, self).__init__(*args, **kwargs)
+# class Test_EmpiricalModel(Test_SuperpositionModel):
+#     def __init__(self, *args, **kwargs):
+#         super(Test_EmpiricalModel, self).__init__(*args, **kwargs)
         
-        # creating class instance for testing
-        self.pm = EmpiricalModel()
+#         # creating class instance for testing
+#         self.pm = EmpiricalModel()
 
-    def test_nonel_nuclei(self):
-        """
-            Test that nonel works
-        """
-        cs_proton = self.pm.cs_proton_grid
-        cs_neutron = self.pm.cs_neutron_grid
+#     def test_nonel_nuclei(self):
+#         """
+#             Test that nonel works
+#         """
+#         cs_proton = self.pm.cs_proton_grid
+#         cs_neutron = self.pm.cs_neutron_grid
         
-        e, cs = self.pm.cs_nonel(1406)
-        cs_mix = 6 * cs_proton + 8 * cs_neutron
-        self.assertTrue(np.all(cs != cs_mix))
+#         e, cs = self.pm.cs_nonel(1406)
+#         cs_mix = 6 * cs_proton + 8 * cs_neutron
+#         self.assertTrue(np.all(cs != cs_mix))
 
 
 if __name__ == '__main__':
